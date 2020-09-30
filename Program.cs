208234764f0a269118b8c7663c340e281230c0e6
marktoledoDEV <markalex.toledo1@gmail.com>
2020-09-30 01:28:09 -0700

    bot is sending messages

--- Program.cs
+++ Program.cs
@@ -12,11 +12,12 @@ namespace big_strong_bot
     class Program
     {
         private static readonly HttpClient HTTPCLIENT = new HttpClient();
-        
+
         //Telegram API Info
         private static readonly string API_TOKEN = "747776578:AAGu66QpI3Ux-x5bBNPo9EUjFHr4YTkv_04";
         private static string test_uri = "https://api.telegram.org/bot747776578:AAGu66QpI3Ux-x5bBNPo9EUjFHr4YTkv_04/getupdates";
-        
+        private static string message_test_uri = "https://api.telegram.org/bot747776578:AAGu66QpI3Ux-x5bBNPo9EUjFHr4YTkv_04/sendmessage?chat_id={0}&text={1}";
+
         static async Task Main(string[] args)
         {
             await ProccessUpdates();
@@ -25,26 +26,40 @@ namespace big_strong_bot
         private static async Task ProccessUpdates()
         {
             Console.WriteLine("This is the Big Strong Bot!");
-             HTTPCLIENT.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HTTPCLIENT.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             HTTPCLIENT.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", API_TOKEN);
-
             HTTPCLIENT.DefaultRequestHeaders.Accept.Clear();
-            var stringTask = HTTPCLIENT.GetStringAsync(test_uri);
-
-            var msg = await stringTask;
-            Console.WriteLine(msg);
 
             var updateRawJson = await HTTPCLIENT.GetAsync(test_uri);
-            if(!updateRawJson.IsSuccessStatusCode)
+            if (!updateRawJson.IsSuccessStatusCode)
             {
                 Console.WriteLine($"GetUpdate Failed. Status Code: {updateRawJson.StatusCode}");
+                return;
             }
 
             Console.WriteLine($"GetUpdate was successful. Parsing JSON now");
             dynamic updateList = JsonConvert.DeserializeObject(await updateRawJson.Content.ReadAsStringAsync());
-            foreach(var result in updateList["result"]){
-                string update_id = result["update_id"];
-                Console.WriteLine($"Sample Update ID: {update_id}");
+            foreach (var result in updateList["result"])
+            {
+                var personData = result["message"]["chat"];
+
+                string first_name = personData["first_name"];
+                string last_name = personData["last_name"];
+                string user_name = personData["username"];
+
+                string message = String.Format("Hello {0} {1}! Would you like me to refer to you as @{2}",
+                                                 personData["first_name"],
+                                                 personData["last_name"],
+                                                 personData["username"]);
+
+                string message_URI = String.Format(message_test_uri, personData["id"], message);
+                var response = await HTTPCLIENT.GetAsync(message_URI);
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Response Failed. Reason: " + response.ReasonPhrase);
+                    return;
+                }
+                Console.WriteLine("Message Should Have been sent");
             }
         }
     }
